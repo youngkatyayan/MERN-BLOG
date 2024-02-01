7edeb1bdde053bddd193f13154f6ae20d28ee7eb
D.Katyayan <142722021+youngkatyayan@users.noreply.github.com>
2024-02-01 22:48:12 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
-
-
-import './App.css'
-
-function App() {
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import Projects from './pages/Projects.jsx'
+const App = () => {
   return (
-    <>
-     <h1>fsda </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+       <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Hohgfdjdfjme</div>
+  )
+}
+
+export default Home
